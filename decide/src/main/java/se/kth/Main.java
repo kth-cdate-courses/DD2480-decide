8ef50f93fcfc4c729d9bc9b29c97af7b32e4617d
Hampus Hallkvist <hampus@hallkvist.org>
2023-01-25 18:12:23 +0100

    fix: Add skeleton project for decide lab

---
+++ decide/src/main/java/se/kth/Main.java
@@ -0,0 +1,11 @@
+package se.kth;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static int square(int x) {
+        return x * x;
+    }
+}
---
+++ decide/src/test/java/se/kth/MainTest.java
@@ -0,0 +1,18 @@
+package se.kth;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    @DisplayName("Square")
+    void square() {
+        assertEquals(4, Main.square(2));
+        assertEquals(4, Main.square(-2));
+        assertEquals(16, Main.square(4));
+        assertEquals(9, Main.square(3));
+        assertEquals(64, Main.square(8));
+    }
+}
