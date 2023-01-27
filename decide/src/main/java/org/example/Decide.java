d0524b565c26fac84b48ac3c1a1080827c5d39ab
Pauline Garelli <garelli@kth.se>
2023-01-27 14:19:22 +0100

    fix: Setup doesn't compile #24
    -add return statements

--- decide/src/main/java/org/example/Decide.java
+++ decide/src/main/java/org/example/Decide.java
@@ -3,6 +3,7 @@ package org.example;
 public class Decide {
 
     private InitialSettings settings;
+
     public Decide(InitialSettings settings) {
         this.settings = settings;
     }
@@ -12,23 +13,68 @@ public class Decide {
     }
 
     public boolean decideHelper() {
-       // TODO do stuff here
+        // TODO do stuff here
+        return true;
     }
 
-    public boolean condition0() {}
-    public boolean condition1() {}
-    public boolean condition2() {}
-    public boolean condition3() {}
-    public boolean condition4() {}
-    public boolean condition5() {}
-    public boolean condition6() {}
-    public boolean condition7() {}
-    public boolean condition8() {}
-    public boolean condition9() {}
-    public boolean condition10() {}
-    public boolean condition11() {}
-    public boolean condition12() {}
-    public boolean condition13() {}
-    public boolean condition14() {}
+    public boolean condition0() {
+        return true;
+    }
+
+    public boolean condition1() {
+        return true;
+    }
+
+    public boolean condition2() {
+        return true;
+    }
+
+    public boolean condition3() {
+        return true;
+    }
+
+    public boolean condition4() {
+        return true;
+    }
+
+    public boolean condition5() {
+        return true;
+    }
+
+    public boolean condition6() {
+        return true;
+    }
+
+    public boolean condition7() {
+        return true;
+    }
+
+    public boolean condition8() {
+        return true;
+    }
+
+    public boolean condition9() {
+        return true;
+    }
+
+    public boolean condition10() {
+        return true;
+    }
+
+    public boolean condition11() {
+        return true;
+    }
+
+    public boolean condition12() {
+        return true;
+    }
+
+    public boolean condition13() {
+        return true;
+    }
+
+    public boolean condition14() {
+        return true;
+    }
 
 }
