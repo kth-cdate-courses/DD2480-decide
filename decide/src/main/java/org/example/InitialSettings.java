7a33d349c75600d7fe7112356ab306e3837c8281
Storsorken <erikwac@kth.se>
2023-01-28 13:21:14 +0100

    feat: add method for calculating triangle area
    Adding this method to the Point class  will make it easier to write methods for condition 3, 10 and 14.
    
    Method follows this formula: https://www.cuemath.com/geometry/area-of-triangle-in-coordinate-geometry/

--- decide/src/main/java/org/example/InitialSettings.java
+++ decide/src/main/java/org/example/InitialSettings.java
@@ -72,5 +72,9 @@ public class InitialSettings {
         public double distance(Point other) {
             return Math.sqrt(Math.pow(x - other.x, 2) + Math.pow(y - other.y, 2));
         }
+
+        public static double triangleArea(Point p1, Point p2, Point p3) {
+            return Math.abs(p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y));
+        }
     }
 }
