ae3219e8299f3a679b43fe5e46f7cb6fc02ac9e7
Storsorken <erikwac@kth.se>
2023-01-28 19:57:24 +0100

    fix: formula for triangle area not dividing by 2

--- decide/src/main/java/org/example/InitialSettings.java
+++ decide/src/main/java/org/example/InitialSettings.java
@@ -74,7 +74,7 @@ public class InitialSettings {
         }
 
         public static double triangleArea(Point p1, Point p2, Point p3) {
-            return Math.abs(p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y));
+            return Math.abs(p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y)) / 2.0;
         }
     }
 }
