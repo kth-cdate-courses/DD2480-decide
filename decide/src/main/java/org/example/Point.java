d30414fd49ebc238adcd6e0f77a2e3eace82576f
Pauline Garelli <garelli@kth.se>
2023-01-29 12:21:47 +0100

    feat: Radius of the smallest circle containing three points #39

--- decide/src/main/java/org/example/Point.java
+++ decide/src/main/java/org/example/Point.java
@@ -27,4 +27,34 @@ public class Point {
         double scalarProduct = (p2.x - x) * (p3.x - x) + (p2.y - y) * (p3.y - y);
         return Math.acos(scalarProduct / (distance(p2) * distance(p3)));
     }
+
+    /**
+     * Computes the radius of the smallest circle containing all three points (p1, p2, p3)
+     * If one angle of the triangle is obtuse:
+     * the smallest circle containing the triangle has the opposite side as diameter
+     * Else:
+     * the smallest circle is the circumscribed circle.
+     * the radius is computed thanks to the law of sines : R = a * b * c / (4 * S)
+     * @param p1 first point
+     * @param p2 second point
+     * @param p3 third point
+     * @return radius of the smallest circle containing p1, p2 and p3
+     */
+    public static double smallestCircleRadius(Point p1, Point p2, Point p3) {
+        // checking if any of the angles is obtuse
+        if (p1.angle(p2, p3) > Math.PI) {
+            return (p2.distance(p3)/2);
+        }
+        if (p2.angle(p1, p3) > Math.PI) {
+            return (p1.distance(p3)/2);
+        }
+        if (p3.angle(p1, p2) > Math.PI) {
+            return (p1.distance(p2)/2);
+        }
+        // else, computing the radius of the circumscribed circle
+        else {
+            return (p2.distance(p3) * p1.distance(p2) * p1.distance(p3) / (4 * triangleArea(p1, p2, p3)));
+        }
+    }
+
 }
