16a7048ac1daa5f759a7accec933403a4d2d98ba
Storsorken <erikwac@kth.se>
2023-01-29 17:44:41 +0100

    feat: add helper method for conditions 10 and 14 #15
    Add helper method that checks whether there exists a set of 3 points forming a triangle fullfilling the area constraint given as an argument.
    
    First two points should be separeted by E_PTS inbetween and last two poitns should be separeted by F_PTS. Se condition 10 or 14 for original formulation of triangle constraint.

--- decide/src/main/java/org/example/Decide.java
+++ decide/src/main/java/org/example/Decide.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 public class Decide {
@@ -116,6 +117,16 @@ public class Decide {
 
     public boolean condition14() {
         return true;
+    }
+
+    private boolean spacedTriangleGivenAreaConstraintExists(Predicate<Double> areaConstraint) {
+        return IntStream.range(0, settings.NUMPOINTS - 2 - settings.PARAMETERS.E_PTS -
+                settings.PARAMETERS.F_PTS).anyMatch(
+                (index) -> areaConstraint.test(
+                        Point.triangleArea(settings.POINTS[index],
+                        settings.POINTS[index + 1 + settings.PARAMETERS.E_PTS],
+                        settings.POINTS[index + 1 + settings.PARAMETERS.E_PTS + 1 + settings.PARAMETERS.F_PTS])
+                ));
     }
 
 }
