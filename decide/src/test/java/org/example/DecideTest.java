6a8c1da7496cef1f465b04be64c3bfa8f9b718e1
EdwardNorberg <norberg.edward@gmail.com>
2023-01-30 23:29:25 +0100

    Add unit tests for condition14
    Two unit tests, one should resolve to true, one should resolve to false.

--- decide/src/test/java/org/example/DecideTest.java
+++ decide/src/test/java/org/example/DecideTest.java
@@ -342,7 +342,29 @@ class DecideTest {
         assertFalse(decide.condition13());
     }
 
+    @Test
+    void condition14_AREA1_equals_1_AREA2_equals_3_EPTS_equals_1_FPTS_equals_1_such_points_exist_TRUE() {
+        int numPoints = 5;
+        Point[] points = { new Point(2,0), new Point(0,0), new Point(0,0), new Point(0, 0), new Point(0,2)};
+        InitialSettings.Parameters parameters = new InitialSettings.Parameters(0, 0, 0, 1, 0, 0, 0, 0, 0, 1 , 0, 0, 0, 1,1, 0, 0, 0, 3);
+        LogicalOperator[][] lcmNotUsed = new LogicalOperator[15][15];
+        boolean[] puvNotUsed = new boolean[15];
+        InitialSettings settings = new InitialSettings(numPoints, points, parameters, lcmNotUsed, puvNotUsed);
+        Decide decide = new Decide(settings);
+        assertTrue(decide.condition14());
+    }
 
+    @Test
+    void condition14_AREA1_equals_1_AREA2_equals_3_EPTS_equals_1_FPTS_equals_1_no_such_points_exist_FALSE() {
+        int numPoints = 5;
+        Point[] points = { new Point(4,0), new Point(0,0), new Point(0,0), new Point(0, 0), new Point(0,4)};
+        InitialSettings.Parameters parameters = new InitialSettings.Parameters(0, 0, 0, 1, 0, 0, 0, 0, 0, 1 , 0, 0, 0, 1,1, 0, 0, 0, 3);
+        LogicalOperator[][] lcmNotUsed = new LogicalOperator[15][15];
+        boolean[] puvNotUsed = new boolean[15];
+        InitialSettings settings = new InitialSettings(numPoints, points, parameters, lcmNotUsed, puvNotUsed);
+        Decide decide = new Decide(settings);
+        assertFalse(decide.condition14());
+    }
 
 
 
