bd75cb358a0f8706d229d823bbc4ecabf29a2ee0
EdwardNorberg <norberg.edward@gmail.com>
2023-01-30 12:23:10 +0100

    Add unit tests for condition1
    Two unit tests, one should resolve to true, and one should resolve to
    false

--- decide/src/test/java/org/example/DecideTest.java
+++ decide/src/test/java/org/example/DecideTest.java
@@ -30,6 +30,30 @@ class DecideTest {
         assertFalse(decide.condition0());
     }
 
+    @Test
+    void condition1_RADIUS1_equals_5_with_3_points_not_inside_circle_TRUE() {
+        int numPoints = 4;
+        Point[] points = {new Point(0,1), new Point(1,0), new Point(0, -1), new Point(5, -12)};
+        InitialSettings.Parameters parameters = new InitialSettings.Parameters(0, 5, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 0, 0, 0,0, 0, 0, 0, 0);
+        LogicalOperator[][] lcmNotUsed = new LogicalOperator[15][15];
+        boolean[] puvNotUsed = new boolean[15];
+        InitialSettings settings = new InitialSettings(numPoints, points, parameters, lcmNotUsed, puvNotUsed);
+        Decide decide = new Decide(settings);
+        assertTrue(decide.condition1());
+    }
+
+    @Test
+    void condition1_RADIUS1_equals_5_with_all_points_inside_circle_FALSE() {
+        int numPoints = 4;
+        Point[] points = {new Point(0,1), new Point(1,0), new Point(0, -1), new Point(3, 4)};
+        InitialSettings.Parameters parameters = new InitialSettings.Parameters(0, 5, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 0, 0, 0,0, 0, 0, 0, 0);
+        LogicalOperator[][] lcmNotUsed = new LogicalOperator[15][15];
+        boolean[] puvNotUsed = new boolean[15];
+        InitialSettings settings = new InitialSettings(numPoints, points, parameters, lcmNotUsed, puvNotUsed);
+        Decide decide = new Decide(settings);
+        assertFalse(decide.condition1());
+    }
+
 
 
 }
