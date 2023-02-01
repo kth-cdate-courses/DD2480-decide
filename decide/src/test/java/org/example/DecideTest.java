b9dc5120533a6c778825265d784a4d19b59fd374
EdwardNorberg <norberg.edward@gmail.com>
2023-02-01 01:51:12 +0100

    Fix: issue #118 add comments for test condition8
    add comments for test contracts. Condition8 corresponds to LIC 8

--- decide/src/test/java/org/example/DecideTest.java
+++ decide/src/test/java/org/example/DecideTest.java
@@ -219,6 +219,8 @@ class DecideTest {
         assertFalse(decide.condition7());
     }
 
+    //Test should resolve to true. Parameters: RADIUS1 = 2, A_PTS = 1, B_PTS = 2.
+    //point2, point4, and point6 cannot be contained in a circle with radius 2. Test corresponds with LIC 8
     @Test
     void condition8_RADIUS1_equals_2_APTS_equals_1_BPTS_equals_1_exists_points_TRUE() {
         int numPoints = 6;
@@ -231,6 +233,8 @@ class DecideTest {
         assertTrue(decide.condition8());
     }
 
+    //Test should resolve to false. Parameters: RADIUS1 = 2, A_PTS = 1, B_PTS = 2.
+    //all points are grouped together in such a way that condition is never met. Test corresponds with LIC 8
     @Test
     void condition8_RADIUS1_equals_2_APTS_equals_1_BPTS_equals_1_doesnt_exists_points_FALSE() {
         int numPoints = 6;
