7de2d6fbed935b47191a40961036eefd03f6f8f8
Storsorken <erikwac@kth.se>
2023-02-01 17:49:36 +0100

    feat: issue #182 handle invalid input for LIC 11

--- decide/src/main/java/org/example/Decide.java
+++ decide/src/main/java/org/example/Decide.java
@@ -230,6 +230,8 @@ public class Decide {
     }
 
     public boolean condition11() {
+        if (!(1 <= settings.PARAMETERS.G_PTS && settings.PARAMETERS.G_PTS <= settings.NUMPOINTS - 2))
+            return false;
         return settings.NUMPOINTS >= 3
                 && IntStream.range(0, settings.NUMPOINTS - 1 - settings.PARAMETERS.G_PTS).anyMatch(
                 (index) -> settings.POINTS[index + settings.PARAMETERS.G_PTS + 1].x - settings.POINTS[index].x < 0);
