c083cf171c0529d8f8ce453f59fc0a5451ec39ba
EdwardNorberg <norberg.edward@gmail.com>
2023-02-01 17:30:40 +0100

    feat: issue #181 add invalid test condition2
    set EPSILON to 2PI

--- decide/src/test/java/org/example/DecideTest.java
+++ decide/src/test/java/org/example/DecideTest.java
@@ -478,6 +478,19 @@ class DecideTest {
         assertTrue(decide.condition1());
     }
 
+    //invalid test EPSILON = 2*PI
+    @Test
+    void InvalidTestCondition2() {
+        int numPoints = 3;
+        Point[] points = {new Point(0,1), new Point(0,0), new Point(1, 0)};
+        InitialSettings.Parameters parameters = new InitialSettings.Parameters(0, 0, 2*Math.PI, 0, 0, 0, 0, 0, 0, 0 , 0, 0, 0, 0,0, 0, 0, 0, 0);
+        LogicalOperator[][] lcmNotUsed = new LogicalOperator[15][15];
+        boolean[] puvNotUsed = new boolean[15];
+        InitialSettings settings = new InitialSettings(numPoints, points, parameters, lcmNotUsed, puvNotUsed);
+        Decide decide = new Decide(settings);
+        assertTrue(decide.condition2());
+    }
+
 }
 
 
