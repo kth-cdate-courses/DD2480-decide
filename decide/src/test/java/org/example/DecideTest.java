a2c8f005cb11c2011f55fa6f3a6bd9d7f3c07872
EdwardNorberg <norberg.edward@gmail.com>
2023-02-01 17:34:53 +0100

    feat: issue #181 invalid test condition4
    QUADS = -1

--- decide/src/test/java/org/example/DecideTest.java
+++ decide/src/test/java/org/example/DecideTest.java
@@ -504,6 +504,21 @@ class DecideTest {
         assertTrue(decide.condition3());
     }
 
+    //invalid test QUADS = 4
+    @Test
+    void InvalidTestCondition4() {
+        int numPoints = 4;
+        Point[] points = {new Point(0, 2), new Point(1,1), new Point(1,-1), new Point(-1, -1)};
+        InitialSettings.Parameters parameters = new InitialSettings.Parameters(0, 0, 0, 0, 3, 4, 0, 0, 0, 0 , 0, 0, 0, 0,0, 0, 0, 0, 0);
+        LogicalOperator[][] lcmNotUsed = new LogicalOperator[15][15];
+        boolean[] puvNotUsed = new boolean[15];
+        InitialSettings settings = new InitialSettings(numPoints, points, parameters, lcmNotUsed, puvNotUsed);
+        Decide decide = new Decide(settings);
+        assertTrue(decide.condition4());
+    }
+
+    
+
 }
 
 
