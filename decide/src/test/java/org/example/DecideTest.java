2e86a88c8d9a202abb985a2e8c0f61f4854b88ed
EdwardNorberg <norberg.edward@gmail.com>
2023-02-01 17:51:41 +0100

    feat: issue #181 invalid input test condition14
    Area2 = -1

--- decide/src/test/java/org/example/DecideTest.java
+++ decide/src/test/java/org/example/DecideTest.java
@@ -620,6 +620,19 @@ class DecideTest {
         assertTrue(decide.condition13());
     }
 
+    //invalid input test AREA2 = -1
+    @Test
+    void InvalidTestCondition14() {
+        int numPoints = 5;
+        Point[] points = { new Point(2,0), new Point(0,0), new Point(0,0), new Point(0, 0), new Point(0,2)};
+        InitialSettings.Parameters parameters = new InitialSettings.Parameters(0, 0, 0, 1, 0, 0, 0, 0, 0, 1 , 0, 0, 0, 1,1, 0, 0, 0, -1);
+        LogicalOperator[][] lcmNotUsed = new LogicalOperator[15][15];
+        boolean[] puvNotUsed = new boolean[15];
+        InitialSettings settings = new InitialSettings(numPoints, points, parameters, lcmNotUsed, puvNotUsed);
+        Decide decide = new Decide(settings);
+        assertTrue(decide.condition14());
+    }
+
 
 }
 
