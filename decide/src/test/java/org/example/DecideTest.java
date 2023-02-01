add439cc53255058ca8e1301f1613276ccd3c72d
EdwardNorberg <norberg.edward@gmail.com>
2023-02-01 17:48:23 +0100

    feat: issue #181 add wrong input test condition12
    length2 = -1

--- decide/src/test/java/org/example/DecideTest.java
+++ decide/src/test/java/org/example/DecideTest.java
@@ -594,6 +594,19 @@ class DecideTest {
         assertTrue(decide.condition11());
     }
 
+    //invalid input test length2 = -1
+    @Test
+    void InvalidTestCondition12() {
+        int numPoints = 4;
+        Point[] points = { new Point(0,0), new Point(2,0), new Point(3,0), new Point(4, 0)};
+        InitialSettings.Parameters parameters = new InitialSettings.Parameters(2, 0, 0, 0, 0, 0, 0, 0, 1, 0 , 0, 0, 0, 0,0, 0, -1, 0, 0);
+        LogicalOperator[][] lcmNotUsed = new LogicalOperator[15][15];
+        boolean[] puvNotUsed = new boolean[15];
+        InitialSettings settings = new InitialSettings(numPoints, points, parameters, lcmNotUsed, puvNotUsed);
+        Decide decide = new Decide(settings);
+        assertTrue(decide.condition12());
+    }
+
 
 }
 
