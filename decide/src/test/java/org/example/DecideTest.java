d270671f8c5416eb8809bd78b9b27c252d4ef5c6
EdwardNorberg <norberg.edward@gmail.com>
2023-02-01 17:32:55 +0100

    feat: issue #181 add invalid test condition3
    AREA1 = -1

--- decide/src/test/java/org/example/DecideTest.java
+++ decide/src/test/java/org/example/DecideTest.java
@@ -491,6 +491,19 @@ class DecideTest {
         assertTrue(decide.condition2());
     }
 
+    //invalid test AREA1 = -1
+    @Test
+    void InvalidTestCondition3() {
+        int numPoints = 4;
+        Point[] points = {new Point(1, 2), new Point(0,2), new Point(0,0), new Point(2, 0)};
+        InitialSettings.Parameters parameters = new InitialSettings.Parameters(0, 0, 0, -1, 0, 0, 0, 0, 0, 0 , 0, 0, 0, 0,0, 0, 0, 0, 0);
+        LogicalOperator[][] lcmNotUsed = new LogicalOperator[15][15];
+        boolean[] puvNotUsed = new boolean[15];
+        InitialSettings settings = new InitialSettings(numPoints, points, parameters, lcmNotUsed, puvNotUsed);
+        Decide decide = new Decide(settings);
+        assertTrue(decide.condition3());
+    }
+
 }
 
 
